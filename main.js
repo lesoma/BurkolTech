eb5f16dacb72367832c46d1c8cda9260a291daae
lesoma <lsoma98@gmail.com>
2023-02-16 17:03:39 +0100

    up

--- main.js
+++ main.js
@@ -71,3 +71,25 @@ function contactAction() {
         form.style.marginLeft = "0";
     }
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("project-image");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "flex";  
+}
