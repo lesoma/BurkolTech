fa829eb7f33015c189a267848624be841fbe7f52
lesoma <lsoma98@gmail.com>
2023-02-16 13:48:38 +0100

    up

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 window.addEventListener("click", hamburgerAction);
 window.addEventListener("click", navLinkAction);
-window.addEventListener("scroll", animationAdder);
-window.addEventListener("load", aboutMeAction);
 window.addEventListener("load", projectAction);
 window.addEventListener("resize", contactAction);
 
@@ -40,60 +38,6 @@ function closeMenu() {
     const navMenu = document.querySelector(".navbar");
     hamburger.classList.remove("active");
     navMenu.classList.remove("responsive");
-}
-
-function animationAdder() {
-    const aboutMeLeft = document.querySelectorAll('.fade-in-left');
-    const aboutMeRight = document.querySelectorAll('.fade-in-right');
-    const projects = document.querySelectorAll('.bounce-in');
-    const subtitles = document.querySelectorAll(".subtitle");
-
-    const appearOptions = {
-        threshold: 0,
-        rootMargin: "0px 0px -200px 0px"
-    }
-
-    const appearOnScroll = new IntersectionObserver(function(entries,appearOnScroll) {
-        entries.forEach(entry => {
-          if (!entry.isIntersecting) {
-            return;
-          } else {
-            entry.target.classList.add('animation');
-            appearOnScroll.unobserve(entry.target);
-          }
-        });
-    }, appearOptions);
-
-    aboutMeLeft.forEach(fader => {
-        appearOnScroll.observe(fader);
-    });
-      
-    aboutMeRight.forEach(fader => {
-        appearOnScroll.observe(fader);
-    });
-      
-    projects.forEach(bouncer => {
-        appearOnScroll.observe(bouncer);
-    });
-      
-    subtitles.forEach(bouncer => {
-        appearOnScroll.observe(bouncer);
-    });
-}
-
-function aboutMeAction() {
-    let infoCard = document.querySelectorAll(".info-card");
-    for (let i = 0; i < infoCard.length; i++) {
-        infoCard[i].addEventListener("click", function() {
-            let infoHeader = infoCard[i].querySelector(".info-header");
-            let cardContent = infoCard[i].querySelector(".card-content");
-            let infoBody = infoCard[i].querySelector(".info-body");
-            infoCard[i].classList.toggle("opened");
-            infoHeader.classList.toggle("removed");
-            cardContent.classList.toggle("opened");
-            infoBody.classList.toggle("opened");
-        });
-    }
 }
 
 function projectAction() {
